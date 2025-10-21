a09ece81ebd89d3b21fe861dc67f9e9a24f48aa8
ElMino94 <trestoux@gaming.tech>
2025-10-22 00:36:47 +0200

    rectification bug sfml animation pas encore tester

--- ShadowSprint/src/core/Animation.cpp
+++ ShadowSprint/src/core/Animation.cpp
@@ -1,33 +1,39 @@
-#include "../core/Animation.h"
+#include "../../include/core/Animation.h"
 
 Animation::Animation()
-	: texture(nullptr), currentFrame(0), frameTime(0.1f), elapsed(0.f){ }
+    : texture(nullptr), currentFrame(0), frameTime(0.1f), elapsed(0.f)
+{
+}
 
 Animation::Animation(sf::Texture& tex, int frameCount, float frameTime, sf::Vector2i frameSize)
-	: texture(&tex), frameTime(frameTime), currentFrame(0), elapsed(0.f) {
-	for (int i = 0; i < frameCount; i++) {
-		frames.emplace_back(i * frameSize.x, 0, frameSize.x, frameSize.y);
-	}
+    : texture(&tex), frameTime(frameTime), currentFrame(0), elapsed(0.f)
+{
+    for (int i = 0; i < frameCount; ++i) {
+        frames.emplace_back(
+            sf::Vector2i(i * frameSize.x, 0),
+            sf::Vector2i(frameSize.x, frameSize.y)
+        );
+    }
 }
 
 void Animation::update(float dt) {
-	if (!texture || frames.empty())return;
+    if (!texture || frames.empty()) return;
 
-	elapsed += dt;
-	if (elapsed >= frameTime) {
-		elapsed = 0.f;
-		currentFrame = (currentFrame + 1) % frames.size();
-	}
+    elapsed += dt;
+    if (elapsed >= frameTime) {
+        elapsed = 0.f;
+        currentFrame = (currentFrame + 1) % frames.size();
+    }
 }
 
 void Animation::applyToSprite(sf::Sprite& sprite) {
-	if (texture && !frames.empty()) {
-		sprite.setTexture(*texture);
-		sprite.setTextureRect(frames[currentFrame]);
-	}
+    if (texture && !frames.empty()) {
+        sprite.setTexture(*texture);
+        sprite.setTextureRect(frames[currentFrame]);
+    }
 }
 
 void Animation::reset() {
-	currentFrame = 0;
-	elapsed = 0.f;
+    currentFrame = 0;
+    elapsed = 0.f;
 }
